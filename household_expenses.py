a2ea4c4ee77d2b75d15b0f80a62abed66b362610
trzyha <39124933+trzyha@users.noreply.github.com>
2019-04-14 22:38:12 +0200

    Add files via upload

--- household_expenses.py
+++ household_expenses.py
@@ -1,30 +1,52 @@
 import tkinter as tk
 from tkinter import ttk
 import datetime
-today_date = datetime.datetime.today().strftime('%d-%m-%Y')
-
+today_date = datetime.datetime.today().strftime('%Y-%m-%d')
 root = tk.Tk()
 root.title("Home spends")
 root.geometry('800x600')
 root.minsize(width=800, height=600)
-
-
+a = 0
+def add_position():
+    if (entry1.get() != '' and entry2.get() != '' and entry3.get() != ''):
+        global a
+        a = a + 1
+        activity = entry1.get()
+        value = entry2.get()
+        date = entry3.get()
+        tree.insert('', 'end', text = a, values=(activity, value, date))
+        clear_entry = tk.StringVar(frame, value = '')
+        #entry1.delete(0, 'end') #from 0 position to end
+        #entry2.delete(0, 'end')
+        
 frame = tk.Frame(root, bg='#80c1ff')
 #frame.place(relx=0.01, rely=0.1, relwidth=0.9, relheight=0.9, width=100)
 frame.place(x=10, y=10, relwidth=0.9, relheight=0.9, width=100)
 
 ttk.Label(frame, text="Activity").grid(row=0, column=0)
-ttk.Label(frame, text="Value [z≈Ç]").grid(row=0, column=2)
+ttk.Label(frame, text="Value [z≥]").grid(row=0, column=2)
 ttk.Label(frame, text="Date").grid(row=0, column=4)
 
-entry1 = ttk.Entry(frame).grid(row=0, column=1)
-entry2 = ttk.Entry(frame).grid(row=0, column=3)
-entry3 = ttk.Entry(frame, textvariable = today_date).grid(row=0, column=5)
-#entry3.insert(0,"cokolwiek")
+entry1 = ttk.Entry(frame)
+entry1.grid(row=0, column=1)
+entry2 = ttk.Entry(frame)
+entry2.grid(row=0, column=3)
+td = tk.StringVar(frame, value = today_date)
+entry3 = ttk.Entry(frame, textvariable = td)
+entry3.grid(row=0, column=5)
 
-button1 = ttk.Button(frame,text='Add').grid(row=0, column=6)
-button2 = ttk.Button(frame,text='Edit').grid(row=0, column=7)
-button3 = ttk.Button(frame,text='Delete').grid(row=0, column=8)
+
+button1 = ttk.Button(frame, text='Add', command = add_position).grid(row=0, column=6)
+button2 = ttk.Button(frame, text='Edit').grid(row=0, column=7)
+button3 = ttk.Button(frame, text='Delete').grid(row=0, column=8)
+
+
+#vsb = ttk.Scrollbar(win, orient="vertical", command=tree.yview)
+#vsb.place(x=30+200+2, y=95, height=200+20)
+#tree.configure(yscrollcommand=vsb.set)
+
+scrollbar = ttk.Scrollbar(frame)
+scrollbar.grid()
 
 tree = ttk.Treeview(frame, height=10, columns=('ID', 'Activities', 'Value[PLN]', 'Date'))
 
@@ -39,13 +61,12 @@ tree.column('#1', stretch=tk.YES, minwidth=50, width=100)
 tree.column('#2', stretch=tk.YES, minwidth=50, width=100)
 tree.column('#3', stretch=tk.YES, minwidth=50, width=100)
 
-tree.insert('', 1, text="1",values=("text","przyk≈Çadowy","01-01-2018"))
-tree.insert('', 2, text="3")
-tree.insert('', 'end', text="2")
+#tree.insert('', 1, text="1", values=("text","przyk≥adowy","01-01-2018"))
+#tree.insert('', 2, text="3")
+#tree.insert('', 'end', text="2")
 
 tree.grid(row=2, column=0, columnspan=9, sticky='NSEW')
 
 
 if __name__ == "__main__":
     root.mainloop()
-
