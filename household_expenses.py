45c0fac9708f311b706410c7c0053b6aeedf0717
trzyha <39124933+trzyha@users.noreply.github.com>
2019-04-18 15:13:53 +0200

    Add files via upload

--- household_expenses.py
+++ household_expenses.py
@@ -8,14 +8,15 @@ root.geometry('800x600')
 root.minsize(width=800, height=600)
 a = 0
 def add_position():
-    if (entry1.get() != '' and entry2.get() != '' and entry3.get() != '' and entry2.get().replace(".", "", 1).isdigit() == True): #isdigit() checks is value digits
+    if (entry1.get() != '' and entry2.get() != '' and entry3.get() != '' 
+        and (entry2.get().replace(".", "", 1).isdigit() == True or entry2.get().replace(",", "", 1).isdigit()) == True): #isdigit() checks is value digits, replace 1 (means one instane is exchanged)
         global a
         a = a + 1
         activity = entry1.get()
-        value = entry2.get()
+        value = float(entry2.get().replace(',','.')) #replacing if comma appears
         date = entry3.get()
         tree.insert('', 'end', text = a, values=(activity, value, date))
-        tree.configure(background='#334353')
+        #tree.configure(background='#334353')
         clear_entry = tk.StringVar(frame, value = '')
         #entry1.delete(0, 'end') #from 0 position to end
         #entry2.delete(0, 'end')
@@ -38,21 +39,16 @@ def delete_position():
         tree.delete(selected_item)
     except:
         pass
-frame = tk.Frame(root, bg='#80c1ff')
-#frame.place(relx=0.01, rely=0.1, relwidth=0.9, relheight=0.9, width=100)
-frame.place(x=10, y=10, relwidth=0.9, relheight=0.9, width=100)
+    
+    
+#class DataTree:
+#    def __init__(self, master):
+        
+        
+        
+        
+        
 
-ttk.Label(frame, text="Activity").grid(row=0, column=0)
-ttk.Label(frame, text="Value [z³]").grid(row=0, column=2)
-ttk.Label(frame, text="Date").grid(row=0, column=4)
-
-entry1 = ttk.Entry(frame)
-entry1.grid(row=0, column=1)
-entry2 = ttk.Entry(frame)
-entry2.grid(row=0, column=3)
-td = tk.StringVar(frame, value = today_date)
-entry3 = ttk.Entry(frame, textvariable = td)
-entry3.grid(row=0, column=5)
 
 
 #------creating menu bar--------
@@ -67,11 +63,32 @@ filemenu.add_command(label = "Save")
 filemenu.add_command(label = "Save As..")
 filemenu.add_command(label = "Exit", command = quit)
 
+button1 = ttk.Button(root, text='Add', command = add_position).grid(row=0, column=6)
+button2 = ttk.Button(root, text='Edit', command = edit_position).grid(row=0, column=7)
+button3 = ttk.Button(root, text='Delete', command = delete_position).grid(row=0, column=8)
+
+entry1 = ttk.Entry(root)
+entry1.grid(row=0, column=1)
+entry2 = ttk.Entry(root)
+entry2.grid(row=0, column=3)
+td = tk.StringVar(root, value = today_date)
+entry3 = ttk.Entry(root, textvariable = td)
+entry3.grid(row=0, column=5)
 
 
-button1 = ttk.Button(frame, text='Add', command = add_position).grid(row=0, column=6)
-button2 = ttk.Button(frame, text='Edit', command = edit_position).grid(row=0, column=7)
-button3 = ttk.Button(frame, text='Delete', command = delete_position).grid(row=0, column=8)
+
+ttk.Label(root, text="Activity").grid(row=0, column=0)
+ttk.Label(root, text="Value [z³]").grid(row=0, column=2)
+ttk.Label(root, text="Date").grid(row=0, column=4)
+
+
+
+#---- frame ----
+frame = tk.Frame(root, bg='#80c1ff')
+#frame.place(relx=0.01, rely=0.1, relwidth=0.9, relheight=0.9, width=100)
+#frame.place(x=10, y=10, relwidth=0.9, relheight=0.9, width=100)
+frame.grid(row=1, column=0, columnspan=8)
+
 
 tree = ttk.Treeview(frame, height=10, columns=('ID', 'Activities', 'Value[PLN]', 'Date'))
 vsb = ttk.Scrollbar(orient="vertical", command=tree.yview)
@@ -87,11 +104,15 @@ tree.column('#1', stretch=tk.YES, minwidth=50, width=100)
 tree.column('#2', stretch=tk.YES, minwidth=50, width=100)
 tree.column('#3', stretch=tk.YES, minwidth=50, width=100)
 
+label1 = ttk.Label(root, text='Sum:')
+label1.grid(row=2,column=0)
+
+
 #tree.insert('', 1, text="1", values=("text","przykładowy","01-01-2018"))
 #tree.insert('', 2, text="3")
 #tree.insert('', 'end', text="2")
 
-tree.grid(row=2, column=0, columnspan=9, sticky='NSEW', in_=frame)
+tree.grid(row=1, column=0, columnspan=9, sticky='NSEW', in_=frame)
 vsb.grid(column=9, row=0, rowspan=3, sticky='ns', in_=frame)
 
 
